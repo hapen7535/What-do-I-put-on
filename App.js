e266fa86d28b643d6006fcee88ebf4ae3583e3a3
Yuhee Kim <dbgml4509@naver.com>
2022-05-12 22:01:56 +0900

    Text 실습

--- App.js
+++ App.js
@@ -114,12 +114,13 @@ import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import LotsOfStyles from './components/StyleEx';
 import  ViewBoxes from './components/ViewEx';
+import TextExample from './components/TextEx';
 
 const App = () => {
   return (
     <View>
 
-    <LotsOfStyles/>
+    <TextExample/>
 
     </View>
   );
